8faadd31d679f33f00e4bec431fb1da0107ec07a
Stefan van den Oord <soord@mac.com>
2016-11-04 21:03:02 +0100

    Initial project

---
+++ RichString/RichString.h
@@ -0,0 +1,19 @@
+//
+//  RichString.h
+//  RichString
+//
+//  Created by Stefan on 04/11/2016.
+//  Copyright © 2016 Stefan van den Oord. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RichString.
+FOUNDATION_EXPORT double RichStringVersionNumber;
+
+//! Project version string for RichString.
+FOUNDATION_EXPORT const unsigned char RichStringVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RichString/PublicHeader.h>
+
+
