21e3bbe2a7399afeaeb3eafd21dd2d141522a26f
Stefan van den Oord <soord@mac.com>
2016-11-05 14:00:28 +0100

    Configured test to use Quick and Nimble

---
+++ RichStringTests/RichStringSpec.swift
@@ -0,0 +1,12 @@
+//  Copyright © 2016 Stefan van den Oord. All rights reserved.
+
+import Quick
+import Nimble
+
+class RichStringSpec: QuickSpec {
+    override func spec() {
+        it("can run tests") {
+
+        }
+    }
+}
--- RichStringTests/RichStringTests.swift
+++
@@ -1,7 +0,0 @@
-//  Copyright © 2016 Stefan van den Oord. All rights reserved.
-
-import XCTest
-
-class RichStringTests: XCTestCase {
-
-}
