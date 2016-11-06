af1f1d103ad10bc2a0de0565f5f58c4114c35db6
Stefan van den Oord <soord@mac.com>
2016-11-06 22:05:34 +0100

    Added the String extension also to NSString

--- RichString/String+Rich.swift
+++ RichString/String+Rich.swift
@@ -20,8 +20,32 @@ extension String: RichText {
     }
 }
 
+extension NSString: RichText {
+    public func bold() -> NSAttributedString {
+        return rich.bold()
+    }
+
+    public func font(_ font: UIFont) -> NSAttributedString {
+        return rich.font(font)
+    }
+
+    public func fontSize(_ size: CGFloat) -> NSAttributedString {
+        return rich.fontSize(size)
+    }
+
+    public func color(_ color: UIColor) -> NSAttributedString {
+        return rich.color(color)
+    }
+}
+
 extension String {
     fileprivate var rich: NSAttributedString {
         return NSAttributedString(string: self)
+    }
+}
+
+extension NSString {
+    fileprivate var rich: NSAttributedString {
+        return NSAttributedString(string: self as String)
     }
 }
