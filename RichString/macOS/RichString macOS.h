b58e508c99b5e4a1eb3e1ff68c661c880cdee615
Stefan van den Oord <soord@mac.com>
2016-11-13 21:17:03 +0100

    Created macOS version of RichString target and configured targets and project structure accordingly

---
+++ RichString/macOS/RichString macOS.h
@@ -0,0 +1,13 @@
+//  Copyright © 2016 Stefan van den Oord. All rights reserved.
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RichString macOS.
+FOUNDATION_EXPORT double RichString_macOSVersionNumber;
+
+//! Project version string for RichString macOS.
+FOUNDATION_EXPORT const unsigned char RichString_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RichString_macOS/PublicHeader.h>
+
+
