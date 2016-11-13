fda3f99fa7ec205e21d644a4d5e0901a76cad1f3
Stefan van den Oord <soord@mac.com>
2016-11-13 23:05:29 +0100

    Added demo content to macOS demo project

--- Demo macOS/ViewController.swift
+++ Demo macOS/ViewController.swift
@@ -1,7 +1,16 @@
 //  Copyright © 2016 Stefan van den Oord. All rights reserved.
 
 import Cocoa
+import RichString
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var label1: NSTextField!
+
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        if let text1 = "This is some text".fontSize(17)?.color(.blue).strikeThrough(style: .styleDouble) {
+            label1.attributedStringValue = text1
+        }
+    }
 }
