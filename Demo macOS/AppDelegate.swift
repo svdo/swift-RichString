f27b4cb6a7457e6620862e2ae0d460efdb160f99
Stefan van den Oord <soord@mac.com>
2016-11-13 21:01:53 +0100

    Created "Demo macOS" app

---
+++ Demo macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Demo macOS
+//
+//  Created by Stefan on 13/11/2016.
+//  Copyright © 2016 Stefan van den Oord. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
