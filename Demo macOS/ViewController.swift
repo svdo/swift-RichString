a988feca08277a5947da2303c0ec64db321850aa
Stefan van den Oord <soord@mac.com>
2016-11-13 21:31:25 +0100

    A bit of cleanup

--- Demo macOS/ViewController.swift
+++ Demo macOS/ViewController.swift
@@ -1,27 +1,7 @@
-//
-//  ViewController.swift
-//  Demo macOS
-//
-//  Created by Stefan on 13/11/2016.
 //  Copyright © 2016 Stefan van den Oord. All rights reserved.
-//
 
 import Cocoa
 
 class ViewController: NSViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
 }
-
