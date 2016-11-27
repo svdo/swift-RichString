a7d4808726d15454f319c694eb13e41af86d9266
Stefan van den Oord <soord@mac.com>
2016-11-27 12:23:31 +0100

    Added tvOS demo

---
+++ Demo tvOS/ViewController.swift
@@ -0,0 +1,38 @@
+//  Copyright © 2016 Stefan van den Oord. All rights reserved.
+
+import UIKit
+import RichString
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
+
+    override func viewWillAppear(_ animated: Bool) {
+        let text1 = "This is some text".fontSize(27).color(.blue).strikeThrough(style: .styleDouble)
+        let text2 = "This is some text".fontSize(27).color(.red).obliqueness(0.5).expansion(0.5)
+        let text3 = text1 + text2.color(.red)
+        let text4 = loremIpsum.paragraphStyle {
+            $0.lineHeightMultiple = 1.2
+            $0.firstLineHeadIndent = 40
+            }.fontSize(32)
+
+        label1.attributedText = text1
+        label2.attributedText = text2
+        label3.attributedText = text3
+        label4.attributedText = text4
+    }
+}
+
+let loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+    "Vivamus malesuada neque eget velit finibus, ac fringilla leo imperdiet. " +
+    "Maecenas ullamcorper sem eros, vitae feugiat arcu ultricies eu. Morbi sit " +
+    "amet diam dictum, auctor est quis, malesuada ipsum. Mauris nisl velit, " +
+    "tempus nec faucibus vitae, varius sit amet ligula. Nullam non lorem sit " +
+    "amet quam convallis finibus a vel ligula. Donec suscipit enim imperdiet " +
+    "ligula auctor, hendrerit mollis urna posuere. Nulla at quam efficitur, " +
+    "rutrum dolor vel, tincidunt nunc. Mauris augue orci, ultrices a sodales " +
+    "euismod, porta ut urna. Maecenas ullamcorper neque non felis elementum " +
+    "aliquet. Nam sit amet tempus dui."
--- Demo watchOS Extension/ExtensionDelegate.swift
+++ Demo watchOS Extension/ExtensionDelegate.swift
@@ -1,10 +1,4 @@
-//
-//  ExtensionDelegate.swift
-//  Demo watchOS Extension
-//
-//  Created by Stefan on 27/11/2016.
 //  Copyright © 2016 Stefan van den Oord. All rights reserved.
-//
 
 import WatchKit
 
