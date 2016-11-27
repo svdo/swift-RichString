67fd7051fbbc4e1b2a59dd80ed6877d949f28bbe
Stefan van den Oord <soord@mac.com>
2016-11-27 11:24:59 +0100

    Added demo for watchOS

---
+++ Demo watchOS Extension/ExtensionDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  ExtensionDelegate.swift
+//  Demo watchOS Extension
+//
+//  Created by Stefan on 27/11/2016.
+//  Copyright © 2016 Stefan van den Oord. All rights reserved.
+//
+
+import WatchKit
+
+class ExtensionDelegate: NSObject, WKExtensionDelegate {
+}
---
+++ Demo watchOS Extension/InterfaceController.swift
@@ -0,0 +1,24 @@
+//  Copyright © 2016 Stefan van den Oord. All rights reserved.
+
+import WatchKit
+import Foundation
+import RichString
+
+class InterfaceController: WKInterfaceController {
+
+    @IBOutlet var label1: WKInterfaceLabel!
+    @IBOutlet var label2: WKInterfaceLabel!
+    @IBOutlet var label3: WKInterfaceLabel!
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+
+        let text1 = "This is some text".fontSize(17).color(.blue).strikeThrough(style: .styleDouble)
+        let text2 = "This is some text".fontSize(17).color(.red).obliqueness(0.5).expansion(0.5)
+        let text3 = text1 + text2.color(.red)
+
+        label1.setAttributedText(text1)
+        label2.setAttributedText(text2)
+        label3.setAttributedText(text3)
+    }
+}
