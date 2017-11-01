c2157b90c7e5a2548776eb65497ce2c9930650a9
Stefan van den Oord <soord@mac.com>
2017-11-01 13:35:43 +0100

    Updated Swift package manager spec (`swift test` doesn't work yet...)

--- Package.swift
+++ Package.swift
@@ -1,10 +1,33 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "RichString",
-    exclude: ["Pods",
-              "Demo_iOS", "Demo_macOS",
-              "RichString/iOS", "RichString/macOS",
-              "RichStringTests",
-              "RichString.xcworkspace", "RichString.xcodeproj"]
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "RichString",
+            targets: ["RichString"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Quick/Nimble.git", from: "7.0.1"),
+        .package(url: "https://github.com/Quick/Quick.git", from: "1.1.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "RichString",
+            dependencies: [],
+            path: "RichString",
+            exclude: ["RichString/iOS", "RichString/tvOS", "RichString/watchOS"]),
+        .testTarget(
+            name: "RichStringTests",
+            dependencies: ["RichString", "Quick", "Nimble"],
+            path: "RichStringTests",
+            exclude: ["RichStringTests/iOS", "RichStringTests/tvOS", "RichStringTests/watchOS"]),
+    ],
+    swiftLanguageVersions: [4]
 )
