a2b2a861f0fb3b7f466c09797be0b1cdb00a06a8
Stefan van den Oord <soord@mac.com>
2017-11-02 12:01:11 +0100

    Cleaned up: swiftlint warnings

--- Package.swift
+++ Package.swift
@@ -6,18 +6,15 @@ import PackageDescription
 let package = Package(
     name: "RichString",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "RichString",
-            targets: ["RichString"]),
+            targets: ["RichString"])
     ],
     dependencies: [
         .package(url: "https://github.com/Quick/Nimble.git", from: "7.0.1"),
-        .package(url: "https://github.com/Quick/Quick.git", from: "1.1.0"),
+        .package(url: "https://github.com/Quick/Quick.git", from: "1.1.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "RichString",
             dependencies: [],
@@ -27,7 +24,7 @@ let package = Package(
             name: "RichStringTests",
             dependencies: ["RichString", "Quick", "Nimble"],
             path: "RichStringTests",
-            exclude: ["RichStringTests/iOS", "RichStringTests/tvOS", "RichStringTests/watchOS"]),
+            exclude: ["RichStringTests/iOS", "RichStringTests/tvOS", "RichStringTests/watchOS"])
     ],
     swiftLanguageVersions: [4]
 )
